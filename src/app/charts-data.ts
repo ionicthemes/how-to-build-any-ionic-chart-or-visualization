6590895898ac0e984bc4a1a68d8e94e782edab7e
Agustin Haller <agustinhaller@gmail.com>
2021-12-10 16:04:33 -0300

    remove unused data

--- src/app/charts-data.ts
+++ src/app/charts-data.ts
@@ -3,22 +3,18 @@ export const CHART_LABELS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 export const CHART_DATA = {
   lastWeek: {
     earnings: {
-      data: [980, 232, 601, 434, 1090, 1230, 1720],
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      data: [980, 232, 601, 434, 1090, 1230, 1720]
     },
     revenue: {
-      data: [120, 699, 1203, 1700, 1200, 1100, 1900],
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      data: [120, 699, 1203, 1700, 1200, 1100, 1900]
     }
   },
   thisWeek: {
     earnings: {
-      data: [680, 932, 901, 934, 1290, 1330, 1320],
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      data: [680, 932, 901, 934, 1290, 1330, 1320]
     },
     revenue: {
-      data: [620, 999, 1003, 1200, 1100, 1200, 1500],
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      data: [620, 999, 1003, 1200, 1100, 1200, 1500]
     }
   }
 };
